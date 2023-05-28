f024ff43814a4abc6819a966bd35f12112614957
1tsKalisey <ramendez2002@gmail.com>
2023-05-28 22:15:42 +0200

    botone

--- pages/Item.js
+++ pages/Item.js
@@ -4,15 +4,86 @@ import { StyleSheet } from "react-native";
 import { paleta } from "../components/Colores";
 import { Image } from "react-native";
 import { useState, useEffect } from "react";
-import { useTranslation } from 'react-i18next';
-import fetchItemData from "../services/ItemApiCall"
+import * as Localization from "expo-localization";
+import i18n from "i18next";
+import { useTranslation, initReactI18next } from "react-i18next";
+import esp from "../public/Dictionary";
+import fetchItemData from "../services/ItemApiCall";
 
 //exp://172.17.40.175:19000
-
+i18n.use(initReactI18next).init({
+  compatibilityJSON: "v3",
+  lng: "es",
+  fallbackLng: "es",
+  resources: {
+    es: {
+      translation: {
+        nombre: "Nombre",
+        datos_identificativos: "Datos identificativos",
+        pais_de_origen: "País de origen",
+        fabricante: "Fabricante",
+        modelo: "Modelo",
+        serie: "Serie",
+        años_de_fabricacion: "Años de fabricación",
+        año_de_inicio: "Año de inicio",
+        año_de_finalizacion: "Año de finalización",
+        clase: "Clase",
+        tipo_de_cuerpo: "Tipo de cuerpo",
+        numero_de_puertas: "Número de puertas",
+        traccion: "Traccion",
+        especificaciones_del_motor: "Especificaciones del motor",
+        fabricante_del_motor: "Fabricante del motor",
+        nombre_del_motor: "Nombre del motor",
+        tipo_de_motor: "Tipo de motor",
+        disposicion_de_los_cilindros: "Disposición de los cilindros",
+        tipo_de_combustible: "tipo de combustible",
+        sistema_de_inyeccion: "Sistema de inyección",
+        tipo_de_aspiracion: "Tipo de aspiración",
+        distribucion_variable_de_valvulas: "Distribución variable de valvulas",
+        valvulas_por_cilindro: "Valvulas por cilindro",
+        valvulas_totales: "Valvulas totales",
+        potencia: "Potencia",
+        unidad_de_potencia: "Unidad de potencia",
+        rpm: "Rpm",
+        par: "Par",
+        unidad_de_par: "Unidad de par",
+        "relacion_peso/potencia": "Relación peso/potencia",
+        dimensiones: "Dimensiones",
+        unidad_de_distancia: "Unidad de distancia",
+        longitud: "Longitud",
+        anchura: "Anchura",
+        altura: "Altura",
+        separacion_de_las_ruedas: "Separación de las ruedas",
+        combustible: "Combustible",
+        unidad_del_combustible: "Unidad del combustible",
+        capacidad_de_combustible: "Capacidad de combustible",
+        pesos: "Pesos",
+        unidad_del_peso: "Unidad del peso",
+        peso_en_vacio: "Peso en vacio",
+        especificaciones_de_la_transmision:
+          "Especificaciones de la transmisión",
+        tipo_de_transmision: "Tipo de transmisión",
+        numero_de_marchas: "Número de marchas",
+        velocidades: "Velocidades",
+        velocidad_maxima: "Velocidad máxima",
+        unidad_de_velocidad: "Unidad de velocidad",
+        limitada: "Limitada",
+        aceleracion: "Aceleración",
+        unidad_de_aceleracion: "Unidad de aceleración",
+        url: "Url",
+      },
+    },
+  },
+  interpolation: {
+    escapeValue: false,
+  },
+  react: {
+    useSuspense: false,
+  },
+});
 //Faltan las imagenes que pertenecen a cada coche(BBDD¿?)
 
 export default function Item({ route, navigation }) {
-
   // Inicializamos los state como estados vacíos y segmentamos la especificaciones
   const { t } = useTranslation();
   const [state, setState] = useState({});
@@ -22,13 +93,12 @@ export default function Item({ route, navigation }) {
   const [Pesos, setPesos] = useState({});
   const [Etransmision, setEtransmision] = useState({});
   const [Velocidades, setVelocidades] = useState({});
-
+  const Separator = () => <View style={styles.separator} />;
   // Variables recibidas por parametro(generalmente desde home)
   const { name, url, ano_inicio, ano_finalizacion, brand } = route.params;
 
   // Utilizamos useEffect para realizar la petición axios al cargar la página
   useEffect(() => {
-
     //Llamada asíncrona a la API
     async function fetchData() {
       const data = await fetchItemData(name);
@@ -81,10 +151,12 @@ export default function Item({ route, navigation }) {
       return (
         <View style={styles.two_column_scroll}>
           <View style={{ width: "100%" }}>
-            <Text style={{ color: paleta.text, fontSize: 19 }}>{item[0]}</Text>
+            <Text style={{ color: paleta.text, fontSize: 19 }}>
+              {t(item[0])}
+            </Text>
             <View
               style={{
-                borderBottomColor: 'white',
+                borderBottomColor: "white",
                 borderBottomWidth: StyleSheet.hairlineWidth,
               }}
             />
@@ -107,39 +179,45 @@ export default function Item({ route, navigation }) {
     if (Map["potencia"]) {
       s.push([
         <View style={{ width: "100%" }}>
-          <Text style={{ color: paleta.text, fontSize: 19 }}>{t('Potencia')}</Text>
+          <Text style={{ color: paleta.text, fontSize: 19 }}>
+            {t("potencia")}
+          </Text>
           <View
             style={{
-              borderBottomColor: 'white',
+              borderBottomColor: "white",
               borderBottomWidth: StyleSheet.hairlineWidth,
             }}
           />
           <Text style={{ color: paleta.fondoT, fontSize: 19 }}>
-            {Map["potencia"]} {Map["unidad_de_potencia"]} / {Map["rpm"]} {t('Rpm')}</Text>
+            {Map["potencia"]} {Map["unidad_de_potencia"]} / {Map["rpm"]}{" "}
+            {t("Rpm")}
+          </Text>
         </View>,
       ]);
     } else if (Map["par"]) {
       s.push([
         <View style={{ width: "100%" }}>
-          <Text style={{ color: paleta.text, fontSize: 19 }}>{t('Par')}</Text>
+          <Text style={{ color: paleta.text, fontSize: 19 }}>{t("Par")}</Text>
           <View
             style={{
-              borderBottomColor: 'white',
+              borderBottomColor: "white",
               borderBottomWidth: StyleSheet.hairlineWidth,
             }}
           />
           <Text style={{ color: paleta.fondoT, fontSize: 19 }}>
-            {Map["par"]} {Map["unidad_de_par"]} / {Map["rpm"]} {t('Par')}
+            {Map["par"]} {Map["unidad_de_par"]} / {Map["rpm"]} {"Par"}
           </Text>
         </View>,
       ]);
     } else if (Map["unidad_del_combustible"]) {
       s.push([
         <View style={{ width: "100%" }}>
-          <Text style={{ color: paleta.text, fontSize: 19 }}>{t('Combustible')}</Text>
+          <Text style={{ color: paleta.text, fontSize: 19 }}>
+            {"Combustible"}
+          </Text>
           <View
             style={{
-              borderBottomColor: 'white',
+              borderBottomColor: "white",
               borderBottomWidth: StyleSheet.hairlineWidth,
             }}
           />
@@ -151,10 +229,10 @@ export default function Item({ route, navigation }) {
     } else if (Map["peso_en_vacio"]) {
       s.push([
         <View style={{ width: "100%" }}>
-          <Text style={{ color: paleta.text, fontSize: 19 }}>{t('Pesos')}</Text>
+          <Text style={{ color: paleta.text, fontSize: 19 }}>{"Pesos"}</Text>
           <View
             style={{
-              borderBottomColor: 'white',
+              borderBottomColor: "white",
               borderBottomWidth: StyleSheet.hairlineWidth,
             }}
           />
@@ -167,11 +245,11 @@ export default function Item({ route, navigation }) {
       s.push([
         <View style={{ width: "100%" }}>
           <Text style={{ color: paleta.text, fontSize: 19 }}>
-          {t('Velocidad maxima')}
+            {"Velocidad maxima"}
           </Text>
           <View
             style={{
-              borderBottomColor: 'white',
+              borderBottomColor: "white",
               borderBottomWidth: StyleSheet.hairlineWidth,
             }}
           />
@@ -186,7 +264,7 @@ export default function Item({ route, navigation }) {
           <Text style={{ color: paleta.text, fontSize: 19 }}>0-100_km/h</Text>
           <View
             style={{
-              borderBottomColor: 'white',
+              borderBottomColor: "white",
               borderBottomWidth: StyleSheet.hairlineWidth,
             }}
           />
@@ -201,10 +279,10 @@ export default function Item({ route, navigation }) {
         for (var key in Map) {
           s.push([
             <View style={{ width: "100%" }}>
-              <Text style={{ color: paleta.text, fontSize: 19 }}>{key}</Text>
+              <Text style={{ color: paleta.text, fontSize: 19 }}>{t(key)}</Text>
               <View
                 style={{
-                  borderBottomColor: 'white',
+                  borderBottomColor: "white",
                   borderBottomWidth: StyleSheet.hairlineWidth,
                 }}
               />
@@ -233,6 +311,19 @@ export default function Item({ route, navigation }) {
               style={styles.image}
             />
           </View>
+          <View style={{ alignItems: "flex-end",marginTop:10 }}>
+            <View style={styles.fixToText}>
+              <Button
+              
+                title="Left button"
+                onPress={() => Alert.alert("Left button pressed")}
+              />
+              <Button
+                title="Right button"
+                onPress={() => Alert.alert("Right button pressed")}
+              />
+            </View>
+          </View>
           <Text style={styles.title}>{state.nombre}</Text>
           <View style={styles.meta}>
             <Text style={styles.author}>{Didentidad.pais_de_origen}</Text>
@@ -240,6 +331,7 @@ export default function Item({ route, navigation }) {
               {ano_inicio}-{ano_finalizacion}
             </Text>
           </View>
+
           <Text style={styles.content}>Descripcion de ejemplo</Text>
 
           <Boxes size={1} bg={"#ffa108"} br={10} clr={"column"}>
@@ -362,5 +454,12 @@ const styles = StyleSheet.create({
     borderBottomWidth: 2,
     borderBottomColor: paleta.fondo,
     marginTop: 10,
+  },
+  fixToText: {
+    width: "30%",
+    height: "auto",
+    justifyContent: "space-between",
+    position: "absolute",
+    flexDirection: "column",
   },
 });
